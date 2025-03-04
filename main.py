d097ee1fcc2fccd9e10db326be6ceb18f96a0855
NoeRicklin <noe.ricklin@outlook.de>
2025-03-05 00:05:54 +0100

    minor changes

--- main.py
+++ main.py
@@ -1,29 +1,19 @@
+from stats import count_words, count_chrs
+import sys
+
+
 def main():
-    with open("books/frankenstein.txt") as f:
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    with open(sys.argv[1]) as f:
         print(f"--- Begin report of {f.name} ---")
         file_contents = f.read()
         num_words = count_words(file_contents)
         print(f"{num_words} words found in the document\n")
         char_dict = count_chrs(file_contents)
         for char, freq in char_dict.items():
-            print(f"The '{char}' character was found {freq} times")
-
-
-def count_words(text):
-    num_words = len(text.split())
-    return num_words
-
-
-def count_chrs(text):
-    dict = {}
-    for char in text:
-        if not char.isalpha():
-            continue
-        if char.lower() in dict:
-            dict[char.lower()] += 1
-        else:
-            dict[char.lower()] = 1
-    return dict
+            print(f"{char}: {freq}")
 
 
 main()
